0edf061b18f309048b958bcbfd447f1887a444cb
Brian Santos <brianmsantos81@gmail.com>
2019-06-03 10:50:25 -0700

    completed app

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,8 +24,8 @@
                                     aria-haspopup="true"
                                     aria-expanded="false">Save and Load <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="">Save Data</a></li>
-                                <li><a href="">Load Data</a></li>
+                                <li><a href="#" @click="saveData">Save Data</a></li>
+                                <li><a href="#" @click="loadData">Load Data</a></li>
                             </ul>
                         </li>
                     </ul>
@@ -50,13 +50,26 @@ export default {
         }
     },
     methods: {
-        ...mapActions([
-            'randomizeStocks'
-        ]),
+        ...mapActions({
+            randomizeStocks: 'randomizeStocks',
+            fetchData: 'loadData'
+        }),
         endDay(){  
             console.log(1);
             this.randomizeStocks();
-        }
+        },
+        saveData() {
+            const data = {
+                funds: this.$store.getters.funds,
+                stockPortfolio: this.$store.getters.stockPortfolio,
+                stocks: this.$store.getters.stocks
+            };
+            this.$http.put('data.json', data); 
+        },
+        loadData(){
+            this.fetchData();
+
+        },
     }
 }
 </script>
